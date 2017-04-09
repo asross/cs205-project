cb8c09562ffe7cc2b54f41ec3d5680aaf684921f
Andrew Ross <andrew@vermonster.com>
2017-04-09 10:35:53 -0400

    efficient/numerically stable (i think) log PDF for mixture grid

--- datasets/gaussian_mixture_grid.py
+++ datasets/gaussian_mixture_grid.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import itertools
+import scipy.misc
+
+class cacheprop(object):
+  def __init__(self, getter): self.getter = getter
+  def __get__(self, actual_self, _):
+    value = self.getter(actual_self)
+    actual_self.__dict__[self.getter.__name__] = value
+    return value
 
 class GaussianMixtureGrid():
   def __init__(self, length, dimensionality, spacing, stddev):
@@ -14,6 +23,20 @@ class GaussianMixtureGrid():
   def sample(self, n):
     return np.array([np.random.normal(self.random_mean(), self.stddev) for _ in range(n)])
 
+  def logp(self, x):
+    individual_logps = ((x - self.means)**2).sum(axis=1) * self.over2sigma2 + self.logZ
+    return scipy.misc.logsumexp(individual_logps)
+
+  @cacheprop
   def means(self):
     return np.array(list(itertools.product(
       *[np.arange(self.length) * self.spacing for _ in range(self.dimensionality)])))
+
+  @cacheprop # normalizing constant
+  def Z(self): return 1. / ((np.sqrt(2*np.pi) * self.stddev)**self.dimensionality)
+
+  @cacheprop # log normalizing constant
+  def logZ(self): return np.log(self.Z)
+
+  @cacheprop # -1 / (2σ²)
+  def over2sigma2(self): return -1. / (2 * self.stddev**2)
