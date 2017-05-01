004c573a9e7f1babd6f39711c9775887c6f38205
Shawn Pan <shawnpan@alum.mit.edu>
2017-05-01 18:39:54 -0400

    Adding parallel metropolis hastings

--- odyssey_setup/teleporting_mcmc/rejection_sample.pyx
+++ odyssey_setup/teleporting_mcmc/rejection_sample.pyx
@@ -3,18 +3,60 @@ from cython.parallel import prange
 import numpy as np
 cimport numpy as np
 from libc.stdlib cimport rand, srand, RAND_MAX
-from libc.math cimport exp as c_exp, sqrt as c_sqrt, M_PI
+#note: we must rename the c math functions. Otherwise, it appears that
+#the python version is used and the program hangs/deadlocks with no
+#error message when run without GIL
+from libc.math cimport exp as c_exp, sqrt as c_sqrt, log as c_log, sin as c_sin, cos as c_cos, M_PI
 
-#normalization constant
-cdef double NORM = 1.0 / c_sqrt(2 * M_PI)
+######################## PRIVATE UTILITIES #################################
+#normalization constants
+cdef double TWOPI = 2 * M_PI
+cdef double ISQRTTWOPI = 1.0 / c_sqrt(TWOPI)
+
+#random double in 0 to 1 range
+cdef inline double rand_uniform() nogil:
+	return float(rand()) / RAND_MAX
 
 #random double in given range
 cdef inline double rand_double(double min_value, double max_value) nogil:
-	return (max_value - min_value) * float(rand()) / RAND_MAX + min_value
+	return (max_value - min_value) * rand_uniform() + min_value
 
-#set the random seed
+#log pdf of normal without normalization constant (MCMC only cares about ratio)
+cdef inline double logp(double x, double mu, double sigma) nogil:
+	cdef double z = (x - mu) / sigma
+	return -0.5 * z * z
+
+#pdf of normal distribution
+cdef inline double normal_pdf(double x, double mu, double sigma) nogil:
+	return ISQRTTWOPI * c_exp(logp(x, mu, sigma)) / sigma
+
+#random sample from normal(0, 1)
+#see https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+#note this is slightly wasteful as it only uses one of the 2 values
+cdef double rand_normal() nogil:
+	cdef:
+		int i1
+		double u1, u2
+	i1 = 0
+	while i1 == 0: #catch the edgecase of log 0
+		i1 = rand()
+	u1 = float(rand()) / RAND_MAX
+	u2 = rand_uniform()
+	return c_sqrt(-2.0 * c_log(u1)) * c_cos(TWOPI * u2)
+
+#############################################################################
+#set the random seed - should be called with separate seed for each MPI node
 cpdef void random_seed(unsigned int seed):
 	srand(seed)
+
+#box-muller sampling of normal(0, 1) -- exposed for testing
+#inputs: number of samples and an empty numpy array to store results
+@boundscheck(False)
+@wraparound(False)
+cpdef void normal_bm(int n_samples, np.float64_t[:] samples) nogil:
+	cdef int i
+	for i in prange(n_samples, schedule="guided"):
+		samples[i] = rand_normal()
 
 #rejection sample from standard normal distribution
 #inputs: number of samples and an empty numpy array to store results
@@ -29,7 +71,30 @@ cpdef void normal(int n_samples, np.float64_t[:] samples) nogil:
 		reject = 1
 		while reject:
 			x = rand_double(-3.0, 3.0)
-			y = rand_double(0.0, 1.0)
-			z = NORM * c_exp(-0.5 * x * x)
+			y = rand_uniform()
+			z = normal_pdf(x, 0, 1)
 			reject = y > z
 		samples[i] = x
+
+#perform parallel metropolis-hastings
+#n_starts: number of chains
+#n_samples: number of samples per chain
+#starts: np array of starts
+#samples: n_starts x n_samples array to store results
+@boundscheck(False)
+@wraparound(False)
+cpdef void normal_mh(int n_starts, int n_samples, np.float64_t[:] starts, np.float64_t[:,:] samples) nogil:
+	cdef:
+		#x value, candidate x value, and corresponding log probabilties
+		double x, xc, lpx, lpxc
+		int i, j
+	for i in prange(n_starts):
+		x = starts[i]
+		lpx = logp(x, 0, 1)
+		for j in range(n_samples):
+			xc = x + 0.2 * rand_normal()
+			lpxc = logp(xc, 0, 1)
+			if rand_uniform() < c_exp(lpxc - lpx):
+				x = xc
+				lpx = lpxc
+			samples[i,j] = x
--- odyssey_setup/teleporting_mcmc/rejection_sample_tests.py
+++ odyssey_setup/teleporting_mcmc/rejection_sample_tests.py
@@ -5,11 +5,39 @@ import pyximport
 pyximport.install()
 import rejection_sample
 
-N_SAMPLES_REJECTION = 100000
+N_SAMPLES = 100000
+
+#Random Seed
 rejection_sample.random_seed(5)
 
-a = np.empty(N_SAMPLES_REJECTION, dtype=np.float64)
-rejection_sample.normal(N_SAMPLES_REJECTION, a)
+#Test Rejection Sampling
+samples = np.empty(N_SAMPLES, dtype=np.float64)
+rejection_sample.normal(N_SAMPLES, samples)
 
-plt.hist(a, bins=100)
+plt.figure()
+plt.hist(samples, bins=100)
+plt.show()
+
+#Test Box-Muller
+samples = np.empty(N_SAMPLES, dtype=np.float64)
+rejection_sample.normal_bm(N_SAMPLES, samples)
+
+plt.figure()
+plt.hist(samples, bins=100)
+plt.show()
+
+#Test Metropolis Hastings
+N_CHAINS = 100
+SAMPLES_PER_CHAIN = N_SAMPLES / N_CHAINS
+starts = np.empty(N_CHAINS, dtype=np.float64)
+rejection_sample.normal(N_CHAINS, starts)
+samples = np.empty((N_CHAINS, SAMPLES_PER_CHAIN), dtype=np.float64)
+rejection_sample.normal_mh(N_CHAINS, SAMPLES_PER_CHAIN, starts, samples)
+
+plt.figure()
+plt.hist(samples.flatten(), bins=100)
+plt.show()
+
+plt.figure()
+plt.hist(samples[0,:], bins=20)
 plt.show()
